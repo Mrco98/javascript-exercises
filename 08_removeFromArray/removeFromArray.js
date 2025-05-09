4308c1c0fc309d28332c0942183d8ef0a49be32f
Mark <marklouistiongco@gmail.com>
2025-05-10 04:04:59 +0800

    Perform instructions from practice 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i=array.length-1; i>=0; i--) {
+        for (const arg of args) {
+            if (array[i] === arg) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
