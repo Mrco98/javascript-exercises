a05283261091239efe81b52b02b2b323eeec5c04
Mark <marklouistiongco@gmail.com>
2025-05-10 02:49:44 +0800

    Perform instruction from practice 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const output = [];
+    for (i=1; i <= stringArray.length; i++) {
+        output[i-1] = stringArray[stringArray.length-i];
+    }
+    return output.join('');
 };
 
 // Do not edit below this line
