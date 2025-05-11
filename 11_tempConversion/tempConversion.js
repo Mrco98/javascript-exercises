f080a89afd1118c3c57cbf1ea9cb7f04be5397e8
Mark <marklouistiongco@gmail.com>
2025-05-11 20:28:04 +0800

    Perform instructions from practice 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  converted = (Number(fahrenheit) - 32) / 1.8; //deg F to deg C formula
+  return +converted.toFixed(1); //convert string to number and limit to one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  converted = Number(celsius)*1.8+32; //deg C to deg F formula
+  return +converted.toFixed(1); // //convert string to number and limit to one decimal place
 };
 
 // Do not edit below this line
